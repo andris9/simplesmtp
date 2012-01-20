83b28fba434dc7e0b06aa7dd4d27e77adec18387
Andris Reinman <andris@node.ee>
2012-01-20 16:29:41 +0200

    update

---
+++ test/smtp.js
@@ -0,0 +1,8 @@
+var testCase = require('nodeunit').testCase;
+
+exports["General tests"] = {
+    "test 1": function(test){
+        test.ok(1, "This is just a sample");
+        test.done();
+    }
+}
