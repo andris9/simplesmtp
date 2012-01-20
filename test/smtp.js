2fb7316de7e22059d00ccfc2fa1b09a36f26854e
Andris Reinman <andris@node.ee>
2012-01-20 22:43:17 +0200

    added tests

--- test/smtp.js
+++ test/smtp.js
@@ -1,8 +1,254 @@
-var testCase = require('nodeunit').testCase;
+var testCase = require('nodeunit').testCase,
+    runClientMockup = require("rai").runClientMockup,
+    SMTPServer = require("../lib/smtp").SMTPServer,
+    netlib = require("net");
+
+var PORT_NUMBER = 8397;
 
 exports["General tests"] = {
-    "test 1": function(test){
-        test.ok(1, "This is just a sample");
-        test.done();
+    setUp: function (callback) {
+        
+        this.smtp = new SMTPServer({
+            SMTPBanner: "SCORPIO",
+            HOSTNAME: "MYRDO",
+            maxSize: 1234
+        });
+        this.smtp.listen(PORT_NUMBER, function(err){
+            if(err){
+                throw err;
+            }else{
+                callback();
+            }
+        });
+        
+    },
+    tearDown: function (callback) {
+        this.smtp.end(callback);
+    },
+    "QUIT": function(test){
+        var cmds = ["QUIT"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("221",resp.toString("utf-8").trim().substr(0,3));
+            test.done();
+        });
+        
+    },
+    "HELO": function(test){
+        var cmds = ["HELO FOO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("250",resp.toString("utf-8").trim().substr(0,3));
+            test.done();
+        });
+        
+    },
+    "HELO fails": function(test){
+        var cmds = ["HELO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "EHLO": function(test){
+        
+        var cmds = ["EHLO FOO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            resp = resp.toString("utf-8").trim();
+            var lines = resp.split("\r\n");
+            for(var i=0; i<lines.length-1; i++){
+                test.equal("250-", lines[i].substr(0,4));
+            }
+            test.equal("250 ", lines[i].substr(0,4));
+            test.done();
+        });
+    },
+    "EHLO fails": function(test){
+        var cmds = ["EHLO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "HELO after STARTTLS": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "HELO FOO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("250",resp.toString("utf-8").trim().substr(0,3));
+            test.done();
+        });
+    },
+    "HELO fails after STARTTLS": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "HELO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "EHLO after STARTTLS": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "HELO FOO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            resp = resp.toString("utf-8").trim();
+            var lines = resp.split("\r\n");
+            for(var i=0; i<lines.length-1; i++){
+                test.equal("250-", lines[i].substr(0,4));
+            }
+            test.equal("250 ", lines[i].substr(0,4));
+            test.done();
+        });
+    },
+    "EHLO fails after STARTTLS": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH fails if not required": function(test){
+        var cmds = ["EHLO FOO", "AUTH LOGIN"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH fails if not required TLS": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "AUTH LOGIN"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "Custom Greeting banner": function(test){        
+        var client = netlib.connect(PORT_NUMBER, function(){
+            client.on("data", function(chunk){
+                test.equal("SCORPIO", (chunk || "").toString().trim().split(" ").pop());
+                client.end();
+            });
+            client.on('end', function() {
+                test.done();
+            });
+        });
+    },
+    "HELO HOSTNAME": function(test){
+        var cmds = ["HELO FOO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("MYRDO",resp.toString("utf-8").trim().substr(4).split(" ").shift());
+            test.done();
+        });
+    },
+    "EHLO HOSTNAME": function(test){
+        var cmds = ["EHLO FOO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("MYRDO",resp.toString("utf-8").trim().substr(4).split(" ").shift());
+            test.done();
+        });
+    },
+    "MAXSIZE": function(test){
+        var cmds = ["EHLO FOO"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.ok(resp.toString("utf-8").trim().match(/^250[\- ]SIZE 1234$/mi));
+            test.done();
+        });
+    }
+}
+
+exports["Require AUTH"] = {
+    setUp: function (callback) {
+        
+        this.smtp = new SMTPServer({requireAuthentication: true});
+        this.smtp.listen(PORT_NUMBER, function(err){
+            if(err){
+                throw err;
+            }else{
+                callback();
+            }
+        });
+        
+        this.smtp.on("authorizeUser", function(envelope, username, password, callback){
+            callback(null, username=="andris" && password=="test");
+        });
+        
+    },
+    tearDown: function (callback) {
+        this.smtp.end(callback);
+    },
+    "Unknown AUTH": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH CRAM"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH fails before STARTTLS": function(test){
+        var cmds = ["EHLO FOO", "AUTH LOGIN"];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH LOGIN Invalid login": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH LOGIN", 
+                    new Buffer("inv").toString("base64"),
+                    new Buffer("alid").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH LOGIN Invalid username": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH LOGIN", 
+                    new Buffer("inv").toString("base64"),
+                    new Buffer("test").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH LOGIN Invalid password": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH LOGIN", 
+                    new Buffer("andris").toString("base64"),
+                    new Buffer("alid").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH LOGIN Login success": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH LOGIN", 
+                    new Buffer("andris").toString("base64"),
+                    new Buffer("test").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("2",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH PLAIN Invalid login": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH PLAIN"+
+                    new Buffer("inv\u0000inv\u0000alid").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH PLAIN Invalid user": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH PLAIN"+
+                    new Buffer("inv\u0000inv\u0000test").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH PLAIN Invalid password": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH PLAIN"+
+                    new Buffer("andris\u0000andris\u0000alid").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
+    },
+    "AUTH PLAIN Login success": function(test){
+        var cmds = ["EHLO FOO", "STARTTLS", "EHLO FOO", "AUTH PLAIN"+
+                    new Buffer("andris\u0000andris\u0000test").toString("base64")];
+        runClientMockup(PORT_NUMBER, "localhost", cmds, function(resp){
+            test.equal("5",resp.toString("utf-8").trim().substr(0,1));
+            test.done();
+        });
     }
 }
