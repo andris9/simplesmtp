8d355549a664647189cccffff33febe23b787dfb
Andris Reinman <andris@node.ee>
2012-02-02 15:20:40 +0200

    Added client

---
+++ index.js
@@ -0,0 +1,4 @@
+
+// expose the API to the world
+module.exports.createServer = require("./lib/server.js");
+module.exports.connect = require("./lib/client.js");
---
+++ test/client.js
@@ -0,0 +1,152 @@
+var testCase = require('nodeunit').testCase,
+    runClientMockup = require("rai").runClientMockup,
+    simplesmtp = require("../index"),
+    netlib = require("net");
+
+var PORT_NUMBER = 8397;
+
+exports["General tests"] = {
+    setUp: function (callback) {
+        this.server = new simplesmtp.createServer({});
+        this.server.listen(PORT_NUMBER, function(err){
+            if(err){
+                throw err;
+            }else{
+                callback();
+            }
+        });
+        
+    },
+    
+    tearDown: function (callback) {
+        this.server.end(callback);
+    },
+    
+    "Connect and setup": function(test){
+        var client = simplesmtp.connect(PORT_NUMBER, false, {});
+        
+        client.on("idle", function(){
+            // Client is ready to take messages
+            test.ok(true);
+            client.close();
+        });
+        
+        client.on("error", function(err){
+            test.ok(false);
+            test.done();
+        });
+        
+        client.on("end", function(){
+            test.done();
+        });
+    }
+}
+
+exports["Disabled EHLO"] = {
+    setUp: function (callback) {
+        this.server = new simplesmtp.createServer({disableEHLO: true});
+        this.server.listen(PORT_NUMBER, function(err){
+            if(err){
+                throw err;
+            }else{
+                callback();
+            }
+        });
+        
+    },
+    
+    tearDown: function (callback) {
+        this.server.end(callback);
+    },
+    
+    "Connect and setup": function(test){
+        var client = simplesmtp.connect(PORT_NUMBER, false, {});
+        
+        client.on("idle", function(){
+            // Client is ready to take messages
+            test.ok(true);
+            client.close();
+        });
+        
+        client.on("error", function(err){
+            test.ok(false);
+            test.done();
+        });
+        
+        client.on("end", function(){
+            test.done();
+        });
+    }
+}
+
+exports["Authentication needed"] = {
+    setUp: function (callback) {
+        this.server = new simplesmtp.createServer({
+            requireAuthentication: true
+        });
+        
+        this.server.on("authorizeUser", function(envelope, user, pass, callback){
+            callback(null, user=="test1" && pass == "test2");
+        });
+        
+        this.server.listen(PORT_NUMBER, function(err){
+            if(err){
+                throw err;
+            }else{
+                callback();
+            }
+        });
+        
+    },
+    
+    tearDown: function (callback) {
+        this.server.end(callback);
+    },
+    
+    "Auth success": function(test){
+        var client = simplesmtp.connect(PORT_NUMBER, false, {
+            auth: {
+                user: "test1",
+                pass: "test2"
+            }
+        });
+        
+        client.on("idle", function(){
+            // Client is ready to take messages
+            test.ok(true);
+            client.close();
+        });
+        
+        client.on("error", function(err){
+            test.ok(false);
+        });
+        
+        client.on("end", function(){
+            test.done();
+        });
+    },
+    
+    "Auth fails": function(test){
+        var client = simplesmtp.connect(PORT_NUMBER, false, {
+            auth: {
+                user: "test3",
+                pass: "test4"
+            }
+        });
+        
+        client.on("idle", function(){
+            // Client is ready to take messages
+            test.ok(false); // should not occur
+            client.close();
+        });
+        
+        client.on("error", function(err){
+            test.ok(true); // login failed
+            client.close();
+        });
+        
+        client.on("end", function(){
+            test.done();
+        });
+    }
+}
