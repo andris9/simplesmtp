137a12f464952eb6f691571f4b7e65e7f80e9848
Andris Reinman <andris@node.ee>
2012-02-08 15:46:05 +0200

    Added Connection Pooling

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 // expose the API to the world
 module.exports.createServer = require("./lib/server.js");
 module.exports.connect = require("./lib/client.js");
+module.exports.createClientPool = require("./lib/pool.js");
