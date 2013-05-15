14ff42e93999993023d9220e71dc0746e4644cd0
Andris Reinman <andris@kreata.ee>
2013-05-15 16:34:54 +0300

    Allow define STARTTLS options

--- test/client.js
+++ test/client.js
@@ -15,26 +15,26 @@ exports["General tests"] = {
                 callback();
             }
         });
-        
+
     },
-    
+
     tearDown: function (callback) {
         this.server.end(callback);
     },
-    
+
     "Connect and setup": function(test){
         var client = simplesmtp.connect(PORT_NUMBER);
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true);
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
@@ -53,29 +53,28 @@ exports["Secure server"] = {
                 callback();
             }
         });
-        
+
     },
-    
+
     tearDown: function (callback) {
         this.server.end(callback);
     },
-    
+
     "Connect and setup": function(test){
         var client = simplesmtp.connect(PORT_NUMBER, false, {
             secureConnection: true
         });
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true);
             client.close();
         });
-        
+
         client.on("error", function(err){
-            console.log(err);
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
@@ -92,26 +91,26 @@ exports["Disabled EHLO"] = {
                 callback();
             }
         });
-        
+
     },
-    
+
     tearDown: function (callback) {
         this.server.end(callback);
     },
-    
+
     "Connect and setup": function(test){
         var client = simplesmtp.connect(PORT_NUMBER, false, {});
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true);
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
@@ -123,11 +122,11 @@ exports["Authentication needed"] = {
         this.server = new simplesmtp.createServer({
             requireAuthentication: true
         });
-        
+
         this.server.on("authorizeUser", function(envelope, user, pass, callback){
             callback(null, user=="test1" && pass == "test2");
         });
-        
+
         this.server.listen(PORT_NUMBER, function(err){
             if(err){
                 throw err;
@@ -135,13 +134,13 @@ exports["Authentication needed"] = {
                 callback();
             }
         });
-        
+
     },
-    
+
     tearDown: function (callback) {
         this.server.end(callback);
     },
-    
+
     "Auth success": function(test){
         var client = simplesmtp.connect(PORT_NUMBER, false, {
             auth: {
@@ -149,22 +148,22 @@ exports["Authentication needed"] = {
                 pass: "test2"
             }
         });
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true);
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
     },
-    
+
     "Auth fails": function(test){
         var client = simplesmtp.connect(PORT_NUMBER, false, {
             auth: {
@@ -172,17 +171,17 @@ exports["Authentication needed"] = {
                 pass: "test4"
             }
         });
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(false); // should not occur
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(true); // login failed
         });
-        
+
         client.on("end", function(){
             test.done();
         });
@@ -195,20 +194,20 @@ exports["Message tests"] = {
             validateSender: true,
             validateRecipients: true
         });
-        
+
         this.server.on("validateSender", function(envelope, email, callback){
             callback(email != "test@pangalink.net"?new Error("Failed sender") : null);
         });
-        
+
         this.server.on("validateRecipient", function(envelope, email, callback){
             callback(email.split("@").pop() != "pangalink.net"?new Error("Failed recipient") : null);
         });
-        
+
         this.server.on("dataReady", function(envelope, callback){
             callback(null, "ABC1"); // ABC1 is the queue id to be advertised to the client
             // callback(new Error("That was clearly a spam!"));
         });
-        
+
         this.server.listen(PORT_NUMBER, function(err){
             if(err){
                 throw err;
@@ -216,22 +215,22 @@ exports["Message tests"] = {
                 callback();
             }
         });
-        
+
     },
-    
+
     tearDown: function (callback) {
         this.server.end(callback);
     },
-    
+
     "Set envelope success": function(test){
         test.expect(2);
-        
+
         var client = simplesmtp.connect(PORT_NUMBER, false, {});
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for envelope
-            
+
             client.useEnvelope({
                 from: "test@pangalink.net",
                 to: [
@@ -240,31 +239,31 @@ exports["Message tests"] = {
                 ]
             });
         });
-        
+
         client.on("message", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for message
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
     },
-    
+
     "Set envelope fails for sender": function(test){
         test.expect(2);
-        
+
         var client = simplesmtp.connect(PORT_NUMBER, false, {});
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for envelope
-            
+
             client.useEnvelope({
                 from: "test3@pangalink.net",
                 to: [
@@ -273,31 +272,31 @@ exports["Message tests"] = {
                 ]
             });
         });
-        
+
         client.on("message", function(){
             // Client is ready to take messages
             test.ok(false); // waiting for message
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(true);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
     },
-    
+
     "Set envelope fails for receiver": function(test){
         test.expect(2);
-        
+
         var client = simplesmtp.connect(PORT_NUMBER, false, {});
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for envelope
-            
+
             client.useEnvelope({
                 from: "test@pangalink.net",
                 to: [
@@ -306,31 +305,31 @@ exports["Message tests"] = {
                 ]
             });
         });
-        
+
         client.on("message", function(){
             // Client is ready to take messages
             test.ok(false); // waiting for message
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(true);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
     },
-    
+
     "Set envelope partly fails": function(test){
         test.expect(3);
-        
+
         var client = simplesmtp.connect(PORT_NUMBER, false, {});
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for envelope
-            
+
             client.useEnvelope({
                 from: "test@pangalink.net",
                 to: [
@@ -339,36 +338,36 @@ exports["Message tests"] = {
                 ]
             });
         });
-        
+
         client.on("rcptFailed", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for message
         });
-        
+
         client.on("message", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for message
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
     },
-    
+
     "Send message success": function(test){
         test.expect(3);
-        
+
         var client = simplesmtp.connect(PORT_NUMBER, false, {});
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for envelope
-            
+
             client.useEnvelope({
                 from: "test@pangalink.net",
                 to: [
@@ -377,38 +376,38 @@ exports["Message tests"] = {
                 ]
             });
         });
-        
+
         client.on("message", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for message
-            
+
             client.write("From: abc@pangalink.net\r\nTo:cde@pangalink.net\r\nSubject: test\r\n\r\nHello World!");
             client.end();
         });
-        
+
         client.on("ready", function(success){
             test.ok(success);
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
     },
-    
+
     "Stream message": function(test){
         test.expect(3);
-        
+
         var client = simplesmtp.connect(PORT_NUMBER, false, {});
-        
+
         client.once("idle", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for envelope
-            
+
             client.useEnvelope({
                 from: "test@pangalink.net",
                 to: [
@@ -417,27 +416,27 @@ exports["Message tests"] = {
                 ]
             });
         });
-        
+
         client.on("message", function(){
             // Client is ready to take messages
             test.ok(true); // waiting for message
-            
+
             // pipe file to client
             fs.createReadStream(__dirname+"/testmessage.eml").pipe(client);
         });
-        
+
         client.on("ready", function(success){
             test.ok(success);
             client.close();
         });
-        
+
         client.on("error", function(err){
             test.ok(false);
         });
-        
+
         client.on("end", function(){
             test.done();
         });
     }
-    
+
 };
