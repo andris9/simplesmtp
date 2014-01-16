4a2351cea49d00d1db7a882b0125f54dfa9d65e6
Andris Reinman <andris@kreata.ee>
2014-01-16 14:23:34 +0200

    Allow responding dataReady events with custom error message

--- examples/size.js
+++ examples/size.js
@@ -3,22 +3,34 @@ var simplesmtp = require("../index"),
 
 // Example for http://tools.ietf.org/search/rfc1870
 
-var allowedRecipientDomains = ["node.ee", "neti.ee"];
+var maxMessageSize = 10;
 
 var smtp = simplesmtp.createServer({
-    maxSize: 5000, // maxSize must be set in order to support SIZE
-    disableDNSValidation: true
+    maxSize: maxMessageSize, // maxSize must be set in order to support SIZE
+    disableDNSValidation: true,
+    debug: true
 });
 smtp.listen(25);
 
+// Set up sender validation function
+smtp.on("validateSender", function(connection, email, done){
+    console.log(1, connection.messageSize, maxMessageSize)
+    // SIZE value can be found from connection.messageSize
+    if(connection.messageSize > maxMessageSize){
+        var err = new Error("Max space reached");
+        err.SMTPResponse = "452 This server can only accept messages up to " + maxMessageSize + " bytes";
+        done(err);
+    }else{
+        done();
+    }
+});
+
 // Set up recipient validation function
 smtp.on("validateRecipient", function(connection, email, done){
-
-    // SIZE value can be found from connection.messageSize
-    if(typeof connection.messageSize == "number" && connection.messageSize > 100){
+    // Allow only messages up to 100 bytes
+    if(connection.messageSize > 100){
         var err = new Error("Max space reached");
-        // anything in the SMTPResponse will be reported back to user. If not set, default error message wil, be used 
-        err.SMTPResponse = "452 Insufficient channel storage: " + email;
+        err.SMTPResponse = "552 Channel size limit exceeded: " + email;
         done(err);
     }else{
         done();
@@ -26,18 +38,28 @@ smtp.on("validateRecipient", function(connection, email, done){
 });
 
 smtp.on("startData", function(connection){
+    connection.messageSize = 0;
     connection.saveStream = fs.createWriteStream("/tmp/message.txt");
 });
 
 smtp.on("data", function(connection, chunk){
+    connection.messageSize += chunk.length;
     connection.saveStream.write(chunk);
 });
 
 smtp.on("dataReady", function(connection, done){
     connection.saveStream.end();
-    done();
 
-    console.log("Delivered message by " + connection.from +
-        " to " + connection.to.join(", ") + ", sent from " + connection.host +
-        " (" + connection.remoteAddress + ")");
+    // check if message
+    if(connection.messageSize > maxMessageSize){
+        // mail was too big and therefore ignored
+        var err = new Error("Max fileSize reached");
+        err.SMTPResponse = "552 message exceeds fixed maximum message size";
+        done(err);
+    }else{
+        done();
+        console.log("Delivered message by " + connection.from +
+            " to " + connection.to.join(", ") + ", sent from " + connection.host +
+            " (" + connection.remoteAddress + ")");
+    }
 });
